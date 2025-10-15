c03d1216f9eb7f8280e8248b5d91856cfc63fdd1
Botir Khaltaev <btrghstk@gmail.com>
2025-10-15 10:55:02 +0100

    feat(analytics): add analytics script to track website usage and performance

---
+++ analytics.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://analytics.llmadaptive.uk/script.js';
+  script.setAttribute('data-website-id', '9b1bdb5e-0f02-4e52-a6a9-84f0db7598bd');
+  document.head.appendChild(script);
+})();
